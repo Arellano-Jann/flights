2f6ac065db867a191b8a33a3953edd1c6a77571b
Your Name <name@domain.example>
2023-11-28 15:03:59 -0800

    route 1.2 done

--- first_number_data_modified.py
+++ first_number_data_modified.py
@@ -10,15 +10,30 @@
 
 
 
-# ROUTE 1
+# ROUTE 1.1
 # SJO -> MCO -> AUS
 # ['ATL', 'AUS', 'BOS', 'BUF', 'BWI', 'CUN', 'DAL', 'DCA', 'DEN', 'ELP', 'HNL', 'HOU', 'IAD', 'IAH', 'JAX', 'LAS', 'LGA', 'MBJ', 'MDW', 'MSY', 'OAK', 'ORD', 'PDX', 'PHL', 'PHX', 'RNO', 'SAN', 'SEA', 'SFO', 'SJC', 'SMF']
 # ['', '', ['ELP', 'HOU', 'AUS',], 'ATL', 'LGA', 'MDW', 'SEA', ['OAK', 'LAS']]
 # ['DEN', 'MSY', 'PDX']
 
+# ROUTE 1.2
+# 802 + OAK -> RNO
+# 206 -> 59  -> 60  ->  81 -> 60  -> 64  -> 78  -> 46  -> 95  -> 53  -> OAK -> RNO
+# SJO -> MCO -> MSY -> AUS -> ATL -> LGA -> MDW -> BOS -> BWI -> SEA -> OAK -> RNO
+# ['BUF', 'CUN', 'DAL', 'DCA', 'DEN', 'ELP', 'HNL', 'HOU', 'IAD', 'IAH', 'JAX', 'LAS', 'LGA', 'MBJ', 'MSY', 'ORD', 'PDX', 'PHL', 'PHX', 'SAN', 'SFO', 'SJC', 'SMF']
+# [['ELP', 'HOU', ['LAS']]
+# ['DEN', 'PDX']
+
+# ROUTE 1.3
+# SJO -> MCO -> MSY -> AUS -> ELP -> DEN ->
+# 206  -> 59  -> 60  -> 00  -> 00  -> 00 -> 00  -> 00  -> 00  -> OAK -> RNO
+# ['ATL', '', 'BOS', 'BUF', 'BWI', 'CUN', 'DAL', 'DCA', 'DEN', 'ELP', 'HNL', 'HOU', 'IAD', 'IAH', 'JAX', 'LAS', 'LGA', 'MBJ', 'MDW', 'MSY', 'OAK', 'ORD', 'PDX', 'PHL', 'PHX', 'RNO', 'SAN', 'SEA', 'SFO', 'SJC', 'SMF']
+# ['', '', ['ELP', 'HOU', '',], 'ATL', 'LGA', 'MDW', 'SEA', ['OAK', 'LAS']]
+# ['DEN', '', 'PDX']
+
 # ROUTE 2.1
 # 725 + OAK -> RNO
-# 199  -> 77  -> 60  -> 64  -> 78  -> 46 -> 95  -> 53  -> 53  -> OAK -> RNO
+# 199  -> 77  -> 60  -> 64  -> 78  -> 46 -> 95  -> 53  -> OAK -> RNO
 # SJO -> HOU -> ATL -> LGA -> MDW -> BOS -> BWI -> SEA -> OAK -> RNO
 # ['AUS', 'BUF', 'CUN', 'DAL', 'DCA', 'DEN', 'ELP', 'HNL', 'IAD', 'IAH', 'JAX', 'LAS', 'MBJ', 'MCO', 'MSY', 'ORD', 'PDX', 'PHL', 'PHX', 'SAN', 'SFO', 'SJC', 'SMF']
 # ['MCO', ['ELP', 'AUS',], ['LAS']]
