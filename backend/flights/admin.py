176dcc1755b3fb49402c56e443db8552fc25d55c
Your Name <name@domain.example>
2023-12-07 15:53:33 -0800

    added flight model to admin page

--- backend/flights/admin.py
+++ backend/flights/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-from .models import Agent, Protocol, Endpoint, Task, TaskResult, Credential, File, Log
+from flights.models import Flight
 
 # Register your models here.
-# admin.site.register(Agent)
-# admin.site.register(Protocol)
-# admin.site.register(Endpoint)
-# admin.site.register(Task)
-# admin.site.register(TaskResult)
-# admin.site.register(Credential)
-# admin.site.register(File)
-# admin.site.register(Log)
+admin.site.register(Flight)
