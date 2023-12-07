e74e9de125a3c2cf209ec79bf7ddffd40b2e3b26
Your Name <name@domain.example>
2023-12-07 15:51:15 -0800

    finished making model

--- backend/flights/models.py
+++ backend/flights/models.py
@@ -8,12 +8,24 @@ from django.conf import settings
 
 
 class Flight(models.Model):
-    name = models.CharField(
-        max_length=100, unique=True, help_text="Human-readable name for the agent."
-    )
-    # Local path to agent definition root
-    definition_path = models.FilePathField(allow_folders=True, allow_files=True)
-    definition_path = models.FileField(upload_to=None, max_length=100)
+    min_cost = models.IntegerField(help_text="Minimum cost for the flight")
+    max_cost = models.IntegerField(help_text="Maximum cost for the flight")
+    avg_cost = models.IntegerField(blank=True, null=True, help_text="Average cost for the flight. If empty, this is autofilled.")
+
+    # This can be extended into a dropdown list of values and autopopulated. However it's manual currently
+    from_city = models.TextField(blank=True, null=True, help_text="Enter source city name")
+    to_city = models.TextField(blank=True, null=True, help_text="Enter destination city name")
+    from_airport = models.CharField(max_length=3, help_text="Enter three letter airport location ID")
+    to_airport = models.CharField(max_length=3, help_text="Enter three letter airport location ID")
+    
+    airline = models.TextField(blank=True, null=True, help_text="")
+    flight_code = models.TextField(blank=True, null=True, help_text="Enter airline's unique flight code")
+    
+    date_first_checked = models.DateField(blank=True, null=True, editable=False, help_text="Enter in the form: ")
+    date_last_checked = models.DateField(blank=True, null=True, help_text="Enter in the form: ")
+    date_of_flight = models.DateField(blank=True, null=True, help_text="Enter the day the plane flies out") # note that this not consider return flights and those would have to be entered again
+    days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"] 
+    day_of_flight = CharField(max_length=9, choices=days_of_the_week, help_text="i.e. Monday")
 
     def get_absolute_url(self):
         return reverse("agent-detail", args=[str(self.id)])
@@ -21,42 +33,29 @@ class Flight(models.Model):
     def __str__(self):
         return self.name
     
-
-class Endpoint(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # Human-readable name, device hostname, and remote address
-    # Does this device actually have an agent installed?
-    is_virtual = models.BooleanField()
-    # What protocol and agent does this endpoint use, if any?
-    # Also, block endpoints from destruction if an agent or protocol is deleted
-    agent = models.ForeignKey(Agent, on_delete=models.PROTECT, related_name="endpoints")
-    protocols = models.ManyToManyField(
-        Protocol, related_name="endpoints"
-    )
-    # Additional JSON configuration object
-    agent_cfg = models.JSONField(blank=True, null=True)
-    # What other endpoints does this endpoint have direct access to?
-    upstream_connections = models.ManyToManyField("self")
-    downstream_connections = models.ManyToManyField("self")
+# has list of airports in the city
+# class City(models.Model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     # Human-readable name, device hostname, and remote address
+#     # Does this device actually have an agent installed?
+#     is_virtual = models.BooleanField()
+#     # What protocol and agent does this endpoint use, if any?
+#     # Also, block endpoints from destruction if an agent or protocol is deleted
+#     agent = models.ForeignKey(Agent, on_delete=models.PROTECT, related_name="endpoints")
+#     protocols = models.ManyToManyField(
+#         Protocol, related_name="endpoints"
+#     )
+#     # Additional JSON configuration object
+#     agent_cfg = models.JSONField(blank=True, null=True)
+#     # What other endpoints does this endpoint have direct access to?
+#     upstream_connections = models.ManyToManyField("self")
+#     downstream_connections = models.ManyToManyField("self")
     
 
-class Task(models.Model):
-    # What user and endpoint, if any, is this task associated with?
-    # Theoretically, every task was caused by *someone*, even if it's
-    # a periodic task
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="tasks")
-    # When was this task started/finished?
-    end_time = models.DateTimeField(blank=True, null=True)
-    # Arbitrary data that may be associated with the task. Should be plaintext.
-    data = models.TextField(blank=True, null=True)
-    
+# class Airport(models.Model):
+#     # What user and endpoint, if any, is this task associated with?
+#     # Theoretically, every task was caused by *someone*, even if it's
+#     # a periodic task
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="tasks")
 
-class TaskResult(models.Model):
-    # Arbitrary data field; may contain files, a raw message, etc. (Note that
-    # if a file is present, it *should* be stored with File and not this field.)
-    data = models.BinaryField(blank=True, null=True)
-
-
-class Log(models.Model):
-    # Enum field, levels are likely to be 0 - 5
-    level = models.IntegerField(blank=True, null=True)
+# class Airline(models.Model):
