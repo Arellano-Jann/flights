21c98efbf5f1347404bc45ebf5e86442c76d4151
Arellano-Jann <jannrellano@nevada.unr.edu>
2024-05-05 18:39:51 -0700

    added urls

--- backend/flights/urls.py
+++ backend/flights/urls.py
@@ -3,7 +3,13 @@ from flights import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
+router.register(r'cities', views.CityViewSet, basename='cities')
+router.register(r'airlines', views.AirlineViewSet, basename='airlines')
+router.register(r'airports', views.AirportViewSet, basename='airports')
+router.register(r'aggregators', views.AggregatorViewSet, basename='aggregators')
 router.register(r'flights', views.FlightViewSet, basename='flights')
+router.register(r'aggflights', views.AggFlightViewSet, basename='aggflights')
+router.register(r'historicaldata', views.HistoricalDataViewSet, basename='historicaldata')
 router.register(r'signUp', views.SignUpViewSet, basename='signUp')
 
 urlpatterns = [
